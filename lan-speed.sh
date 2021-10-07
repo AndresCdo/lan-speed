b4a2dbd6735627564597511779d67de4212b3b3c
andrezz1997 <andrezz1997@gmail.com>
2021-10-06 23:26:34 -0500

    Se agregan ';' al final de todas las lineas y se agrega el comando 'rm'.
    De esta manera se cumplen las buenas practicas de escritura Bash y se elimina el historial de consulta.

--- lan-speed.sh
+++ lan-speed.sh
@@ -3,11 +3,11 @@
 figlet LAN Speed
 
 # Se crea el archivo CSV y se agrega un título a la columna.
-echo "Data" > data.csv
+echo "Data" > data.csv;
 
 # El script tiene dos argmentos: la interfaz de monitoreo y la dirección ip4.
 case $# in
-	2)	
+	2)
 		# Presione cualquier tecla para salir del ciclo.
 		while [ true ] ; do
 			read -t 1 -n 1
@@ -19,11 +19,12 @@ case $# in
 				ping -A -c 300 -D -I $1 -i 1 $2 | cat | awk -F ' ' '{split($8,time,"="); print time[2]}' >> data.csv;
 			fi
 		done
-		python3 analisis.py
+		python3 analisis.py;
+		rm *.csv;
 		echo "Graficando datos...";
-		eog plot.png
+		eog plot.png;
 	;;
 	*)
-		echo "More than one argument is needed."
+		echo "More than one argument is needed.";
 	;;
 esac
