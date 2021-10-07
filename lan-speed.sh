272ab97e08dcf9093a5ab17b5cef58121d6abfc3
andrezz1997 <andrezz1997@gmail.com>
2021-10-06 22:31:44 -0500

    Se modifica el método de medida y se fitran los datos outliers.

--- lan-speed.sh
+++ lan-speed.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-if [ ! $# -eq 0 ]; then
-ping -A -D -I enp3s0 -i 1 $1 | cat | awk -F ' ' '{split($8,time,"="); print time[2]}' > data.csv
-fi
+# Se crea el archivo CSV y se agrega un título a la columna.
+echo "Data" > data.csv
+
+# El script tiene dos argmentos: la interfaz de monitoreo y la dirección ip4.
+case $# in
+	2)
+		# Print timestamp (unix time + microseconds as in gettimeofday) before each line.
+		ping -A -D -I $1 -i 1 $2 | cat | awk -F ' ' '{split($8,time,"="); print time[2]}' >> data.csv
+	;;
+	*)
+		echo "More than one argument is needed."
+	;;
+esac
