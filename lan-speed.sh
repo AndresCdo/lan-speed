934ee0938502f7e33d0f6db4ee71e8f627de16a7
Andres Caicedo <Andrezz1997@gmail.com>
2023-03-09 00:16:03 +0100

    Use pipe file as stdin

--- lan-speed.sh
+++ lan-speed.sh
@@ -2,29 +2,33 @@
 
 figlet LAN Speed
 
-# Create a named pipe instead of a CSV file.
-mkfifo ping_output
+# Create a named pipe in the /tmp directory.
+PIPE=/tmp/ping_output
+rm -f $PIPE
+mkfifo $PIPE
 
-# The script has two arguments: the monitoring interface and the IPv4 address.
+# The script has two arguments: the monitoring interface and the ipv4 address.
 case $# in
-	2)
-		# Print timestamp (unix time + microseconds as in gettimeofday) before each line.
-		ping -A -c 300 -D -I $1 -i 1 $2 | cat | awk -F ' ' '{split($8,time,"="); print time[2]}' > ping_output &
+    2)
+        # Press any key to exit the loop.
+        while [ true ] ; do
+            read -t 1 -n 1
+            if [ $? = 0 ] ; then
+                echo "Analyzing data..."
+                break ;
+            else
+                # Append the ping output to the named pipe.
+                ping -A -c 300 -D -I $1 -i 1 $2 | awk -F ' ' '{split($8,time,"="); print time[2]}' > $PIPE &
+            fi
+        done
 
-		# Wait for 1 second before running the Python script.
-		sleep 1
-		
-		# Pipe the output of the ping command directly to the Python script.
-		cat ping_output | python3 analisis.py
-
-		# Remove the named pipe.
-		rm ping_output
-
-		# Display the plot using the default image viewer.
-		echo "Graficando datos...";
-		# eog plot.png;
-	;;
-	*)
-		echo "Se necesita más de un argumento.";
-	;;
+        # Read the output from the named pipe and pipe it to the Python script.
+        cat $PIPE | python analysis.py
+        rm $PIPE
+        #echo "Plotting data..."
+        #eog plot.png
+        ;;
+    *)
+        echo "More than one argument is needed."
+        ;;
 esac
